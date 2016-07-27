9e82329292fc4a0d1d2e95adf99f7dba92d175ec
LDD07 <LDD07@LDD07.estudiantes.uco>
2016-07-26 20:55:59 -0500

    version 3.0 eje1

--- src/p1p1/P1p1.java
+++ src/p1p1/P1p1.java
@@ -15,7 +15,7 @@ public class P1p1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hey 2.0");
+        System.out.println("hey 3.0");
     }
     
 }
